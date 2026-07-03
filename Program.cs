e48fd47c1792905303089b3c49da117826749845
Deya <deya.ppmg@gmail.com>
2026-07-03 21:12:22 +0300

    Create Menu

--- Program.cs
+++ Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        private const string FilePath = "library.txt";
         static void Main(string[] args)
         {
-            
+            List<Book> library = LoadBooksFromFile();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("--- МЕНЮ БИБЛИОТЕКА ---");
+                Console.WriteLine("1. AddBook Добави книга");
+                Console.WriteLine("2. Списък с всички книги");
+                Console.WriteLine("3. Заеми книга");
+                Console.WriteLine("4. Върни книга");
+                Console.WriteLine("5. Изход: ");
+                Console.WriteLine("Избор: ");
+                string choice = Console.ReadLine();
+
+            }
         }
     }
 }
